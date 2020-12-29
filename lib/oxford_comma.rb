4a6a8598130efeeaa59e84cee02466cc13e39ded
Taylor Treece <tayloratreece@gmail.com>
2020-12-29 01:09:08 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,7 +6,8 @@ def oxford_comma(array)
     return array.join
   else
   i = 0
-  array.each { |n| i++, array[i] = "#{n}, "}
+  array.each { |n| i += 1 }
+    
   array.insert(array.length - 1, " and ")
   return array.join
   end
