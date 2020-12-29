853d03aef88578d70a36e133fbc48c64a1c76cee
Taylor Treece <tayloratreece@gmail.com>
2020-12-29 03:40:50 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return array.join
+  elsif array.length == 2
+    array.insert(array.length - 1, " and ")
+    return array.join
+  else
+  i = 0
+  array[0...-1].collect do |n|
+    array[i] = "#{n}, "
+    i += 1
+    end
+  array.insert(array.length - 1, "and ")
+  return array.join
+  end
 end
