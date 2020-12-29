c685e0f728bb62cae3461af02e0321d8fd953807
Taylor Treece <tayloratreece@gmail.com>
2020-12-29 01:46:45 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -12,7 +12,7 @@ def oxford_comma(array)
     next n if n == array[array.length]
     end
   array.insert(array.length - 1, "and ")
-  array.slice(array.length - 1, -2)
+  array.slice(array.length - 1, 4)
   return array.join
   end
 end
