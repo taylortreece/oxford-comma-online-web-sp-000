c78ce2ec4d7aea4db09a8516f03a1e44d01266c4
Taylor Treece <tayloratreece@gmail.com>
2020-12-29 01:41:24 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -11,7 +11,6 @@ def oxford_comma(array)
     array[i - 1] = "#{n}, "
     i += 1
     end
-  array[array.length - 1] = array[-1]
   array.insert(array.length - 1, "and ")
   return array.join
   end
