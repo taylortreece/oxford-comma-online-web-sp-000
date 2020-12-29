fed89f744c19941e8765684f45228aac2dd0bf87
Taylor Treece <tayloratreece@gmail.com>
2020-12-29 01:41:58 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,9 +6,9 @@ def oxford_comma(array)
     return array.join
   else
   i = 1
-  break if i = array.length - 1
   array.each do |n|
     array[i - 1] = "#{n}, "
+    break if i = array.length - 1
     i += 1
     end
   array.insert(array.length - 1, "and ")
